8f4435f9a307f29ae1d71791f0bfd66a28c9553e
TAHİR MAT <matahir33@gmail.com>
2022-10-19 16:48:15 +0300

    .

--- README.md
+++ README.md
@@ -10,9 +10,12 @@
 
 # HELPERS
 -------------------------------------------------------------------------------------------------------------------------------
+- [Firebase authentication](https://www.youtube.com/watch?v=KLmSfHcOXlc&ab_channel=Avra)
+- [Firebase upload fetaures](https://www.youtube.com/watch?v=uzc3OEjjbn8&ab_channel=Avra)
 - [Deta base configuration](https://www.youtube.com/watch?v=3egaMfE9388&ab_channel=CodingIsFun)
 - [Update the data with session_state referenced button](https://www.youtube.com/watch?v=5l9COMQ3acc&ab_channel=M%C4%B1sraTurp)
 - [Streamlit cache](https://www.youtube.com/watch?v=nF-PQj0k5-o&ab_channel=PythonTutorialsforDigitalHumanities)
+
 
 
 
--- app.py
+++ app.py
@@ -47,19 +47,23 @@ if choice == "Sign Up":
 
     if submit:
         user = auth.create_user_with_email_and_password(email, password)
-        streamlit.success("Your account cretaed")
+        # streamlit.success("Your account cretaed")
 
         # Sign In
         user = auth.sign_in_with_email_and_password(email, password)
         db.child(user["localId"]).child("Handle").set(handle)
         db.child(user["localId"]).child("ID").set(user["localId"])
 
-        streamlit.title("Welcome" + handle)
+
+        streamlit.title("Successfuly Signed Up" + "\n" + handle)
 
 if choice == "Log In":
     login = streamlit.sidebar.checkbox("Login")
     if login:
         user = auth.sign_in_with_email_and_password(email, password)
 
-        bio = streamlit.radio("Jump to", ["Home", "Workplace", "Settings"])
+        streamlit.title("Successfuly Logged In " + "\n" +  user["localId"] + "\t" +  user["email"])
+        # streamlit.write("<style>div.row-widget.stRadio >div{flex-direction:row;}</style>", unsafe_allow_html=True)
+
+        # bio = streamlit.radio("Jump to", ["Home", "Workplace", "Settings"])
         
